3e3b7efe5cf400d9d5bf584d8e4827d8c466401a
ATechnoHazard <amolele@firehound.org>
2018-07-03 03:14:54 +0530

    jenkins: add groovy scripts to switch to pipelines
    Change-Id: I42b0c31f54b5fadb5f1337e8126d1e678ef2ddbc
    Signed-off-by: ATechnoHazard <amolele@firehound.org>

---
+++ fh-build.groovy
@@ -0,0 +1,33 @@
+def basejobname = "fh_" + DEVICE + '-' + BUILD_TYPE
+def BUILD_TREE = "/var/lib/jenkins/workspace/builder"
+
+node("master") {
+	currentBuild.displayName = basejobname
+	
+	stage('Sync') {
+		sh '''#!/bin/bash
+		cd '''+BUILD_TREE+'''
+        repo forall -c "git reset --hard"
+        repo forall -c "git clean -f -d"
+        repo sync -d -c -j32 --force-sync
+      '''
+	}
+	stage('Clean') {
+		sh '''#!/bin/bash
+		cd '''+BUILD_TREE+'''
+		make clean
+		make clobber
+		'''
+	}
+	stage('Build') {
+		sh '''#!/bin/bash +e
+		cd '''+BUILD_TREE+'''
+		. build/envsetup.sh
+		export USE_CCACHE=1
+        export CCACHE_COMPRESS=1
+		export FH_RELEASE=true
+		lunch fh_$DEVICE-$BUILD_TYPE
+		mka bacon
+		'''
+	}
+}
