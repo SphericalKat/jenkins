a3f9ffa2ee0300c6d75d10919e345d93e3714a4d
ATechnoHazard <amolele@firehound.org>
2018-07-06 01:15:27 +0530

    jenkins: cleanup
    Change-Id: If64810244c76d36450c3074473542596d876a276
    Signed-off-by: ATechnoHazard <amolele@firehound.org>

--- builder.groovy
+++ builder.groovy
@@ -7,10 +7,10 @@ node("master") {
 	stage('Sync') {
 		sh '''#!/bin/bash
 		cd '''+BUILD_TREE+'''
-        repo forall -c "git reset --hard"
-        repo forall -c "git clean -f -d"
-        repo sync -d -c -j32 --force-sync
-      '''
+	        repo forall -c "git reset --hard"
+	        repo forall -c "git clean -f -d"
+	        repo sync -d -c -j32 --force-sync
+		'''
 	}
 	stage('Clean') {
 		sh '''#!/bin/bash
@@ -24,7 +24,7 @@ node("master") {
 		cd '''+BUILD_TREE+'''
 		. build/envsetup.sh
 		export USE_CCACHE=1
-        export CCACHE_COMPRESS=1
+		export CCACHE_COMPRESS=1
 		export FH_RELEASE=true
 		lunch fh_$DEVICE-$BUILD_TYPE
 		mka bacon
